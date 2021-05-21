1d769e06c176195beb1612f75d3df921be452bde
Vishnu Pathmanaban <vpathman@andrew.cmu.edu>
2021-05-20 20:17:47 -0400

    spotify html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,4 @@ class HomeController < ApplicationController
     # get home path
     def index
     end 
-
-    # get spotify path
-    def spotify
-    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
     def spotify
       spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
       # Now you can access user's private data, create playlists and much more
-  
+      @email = spotify_user.email
+      @top_artists = spotify_user.top_artists
+      @top_tracks = spotify_user.top_tracks(time_range: 'short_term')
     end
   end
