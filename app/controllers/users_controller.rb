1159713e7b638f43f400f74f309d0d7cb7e66e8e
Vishnu Pathmanaban <vpathman@andrew.cmu.edu>
2021-05-30 20:57:37 -0400

    user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,8 @@ class UsersController < ApplicationController
 
     def spotify
       spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-      # Now you can access user's private data, create playlists and much more
-      User.create(:user_hash => spotify_user.to_hash)
-    end
-
-    def new
-      @user = User.new
-    end
-
-    def create
-      @user = User.new(user_params)
+      @user = User.create(:user_hash => spotify_user.to_hash)
       redirect_to user_path(@user)
-  
-      if @user.save
-        # if saved to database
-        flash[:notice] = "Successfully accessed user information."
-      else 
-        flash[:notice] = "Could not access user information."
-      end
     end
   
     def show 
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
   # home routes
   get 'home', to: 'home#index', as: :home
 
-  # user routes
+  # authorization route
   get '/auth/spotify/callback', to: 'users#spotify'
-  get 'users/:id', to: 'user#show'
 
-  # room routes
-  post 'rooms/new', to: 'rooms#new'
-  get 'rooms/:id', to: 'rooms#show'
+  resources :users
+  resources :rooms
 
   # root @ home
   root 'home#index'
