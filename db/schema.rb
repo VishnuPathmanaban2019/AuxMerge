5a477c6d253b323a7254b7a46a72921a46329492
Vishnu Pathmanaban <vpathman@andrew.cmu.edu>
2021-06-14 21:23:49 -0400

    access saved user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_190933) do
+ActiveRecord::Schema.define(version: 2021_06_15_003106) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "password"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_190933) do
     t.text "user_hash"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "track_room_relations", "rooms"
