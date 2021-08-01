ccf7e8fdd0460244c9a71752f1c10966d4b49930
Vishnu Pathmanaban <vpathman@andrew.cmu.edu>
2021-07-31 22:28:51 -0400

    link fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,8 @@ function show_slow_warning() {
     $("#slow_warning").show();
 }
 
-// $(document).ready(function() {
-//     $('a.link_to_slow_page').click(function(){
-//       show_slow_warning();
-//     });
-// });
+$(document).ready(function() {
+    $('#my-link').click(function(){
+      show_slow_warning();
+    });
+});
