8879776b90b86e72aadff489d646dfe75e8b2dbc
Vishnu Pathmanaban <vpathman@andrew.cmu.edu>
2021-07-31 22:39:28 -0400

    event testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,9 @@ function show_slow_warning() {
     $("#slow_warning").show();
 }
 
-$(document).ready(function() {
+$(document).ready(function(event) {
     $('#my-link').click(function(){
       show_slow_warning();
+      event.preventDefault();
     });
 });
